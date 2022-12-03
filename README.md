fb362329c2451ad0668f3919896b2c7f2138c788
Florian Imdahl <git@ffflorian.de>
2022-12-03 23:15:17 +0100

    feat: Basic features (WIP)

--- README.md
+++ README.md
@@ -1,14 +1,31 @@
-# ts-boilerplate [![Build Status](https://github.com/ffflorian/ts-boilerplate/workflows/Build/badge.svg)](https://github.com/ffflorian/ts-boilerplate/actions/)
+# ntfy [![Build Status](https://github.com/ffflorian/ntfy/workflows/Build/badge.svg)](https://github.com/ffflorian/ntfy/actions/)
 
-A boilerplate for a modern™ TypeScript project.
+Send notifications over [ntfy.sh](https://ntfy.sh).
 
 ## Installation
 
-Fork this repository or just [generate a new one](https://github.com/ffflorian/ts-boilerplate/generate) from this template.
+```
+npm install ntfy
+```
+
+```
+yarn add ntft
+```
 
 ## Usage
 
-- Clone your new repository
-- Run `yarn` in the new repository
-- Start coding
-- Don't forget to update the `package.json`!
+```ts
+import {NtfyClient} from 'ntfy';
+
+const ntfyClient = new NtfyClient();
+
+await ntfyClient.publish({
+  clickURL: 'https://github.com/ffflorian/ntfy',
+  iconURL: 'https://avatars.githubusercontent.com/ffflorian',
+  message: 'Remote access to device detected. Act right away.',
+  priority: 'urgent',
+  tags: ['warning', 'skull'],
+  title: 'Unauthorized access detected',
+  topic: 'mytopic',
+});
+```
--- src/index.ts
+++ src/index.ts
@@ -1,9 +1,79 @@
-/*
- * This is your index file.
- *
- * Example:
- *
- * ```
- * export * from './project';
- * ```
- */
+import axios, {AxiosRequestConfig, AxiosRequestHeaders} from 'axios';
+import {URL} from 'url';
+import {Config, MessagePriority} from './interfaces';
+
+const defaultServerURL = 'https://ntfy.sh';
+
+export class NtfyClient {
+  private readonly serverURL: string;
+
+  /**
+   * @param serverURL Specify your own ntfy Server. See [Self-hosting](https://docs.ntfy.sh/install/).
+   */
+  constructor(serverURL?: string) {
+    this.serverURL = serverURL || defaultServerURL;
+  }
+
+  async publish(config: Omit<Config, 'server'>): Promise<any> {
+    publish({
+      ...config,
+      server: this.serverURL,
+    });
+  }
+}
+
+export async function publish(config: Config): Promise<any> {
+  const axiosConfig: AxiosRequestConfig & {headers: AxiosRequestHeaders} = {
+    headers: {},
+  };
+
+  if (config.actions) {
+    // TODO
+  }
+
+  if (config.authorization) {
+    axiosConfig.withCredentials = true;
+    axiosConfig.auth = config.authorization;
+  }
+
+  if (config.delay) {
+    axiosConfig.headers['X-Delay'] = config.delay;
+  }
+
+  if (config.disableCache) {
+    axiosConfig.headers['X-Cache'] = 'no';
+  }
+
+  if (config.disableFirebase) {
+    axiosConfig.headers['X-Firebase'] = 'no';
+  }
+
+  if (config.emailAddress) {
+    axiosConfig.headers['X-Email'] = config.emailAddress;
+  }
+
+  if (config.fileAttachment) {
+    // TODO
+  }
+
+  if (config.iconURL) {
+    axiosConfig.headers['X-Icon'] = config.iconURL;
+  }
+
+  if (config.priority) {
+    axiosConfig.headers['X-Priority'] = config.priority;
+  }
+
+  if (config.tags) {
+    axiosConfig.headers['X-Tags'] = typeof config.tags === 'string' ? config.tags : config.tags.join(',');
+  }
+
+  if (config.title) {
+    axiosConfig.headers['X-Title'] = config.title;
+  }
+
+  const url = new URL(config.topic, config.server || defaultServerURL);
+
+  const {data} = await axios.post(url.href, config.message, axiosConfig);
+  return data;
+}
